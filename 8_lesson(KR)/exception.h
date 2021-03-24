0bf53ccd0be0b9fd39e88e6c8a91c581df5a20b9
Оберман Игорь <oberman.ie@gmail.com>
2021-03-24 18:33:16 +0300

    Create exception.h

---
+++ 8_lesson(KR)/exception.h
@@ -0,0 +1,21 @@
+using std::string;
+
+class Exception
+{
+    private:
+        string error;
+
+    public:
+        Exception(string s);
+        string what() const noexcept;
+};
+
+Exception::Exception(string s)
+{
+    error = s;
+}
+
+string Exception::what() const noexcept
+{
+    return error;
+}
